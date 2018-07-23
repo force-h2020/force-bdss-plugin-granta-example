7d334edab64618c804db577929518df0b08aa3f4
Stefano Borini <sborini@enthought.com>
2018-07-23 16:58:01 +0100

    Introduced new granta library for uploading (prototype)

--- granta_example/example_notification_listener/example_notification_listener.py
+++ granta_example/example_notification_listener/example_notification_listener.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 import datetime
-import requests
-import granta_example.mipy as mi
-from traits.api import Instance, List
+import granta
+from traits.api import Instance, List, Any
 
 from force_bdss.api import (
     BaseNotificationListener,
@@ -17,14 +16,15 @@ from granta_example.example_notification_listener\
 
 class ExampleNotificationListener(BaseNotificationListener):
     _model = Instance(ExampleNotificationListenerModel)
-    _session = Instance(requests.Session)
-    _values = List
+    _mi = Any()
+    _names = List()
+    _values = List()
 
     def deliver(self, event):
         if isinstance(event, MCOStartEvent):
-            self._values = []
+            self._names = event.input_names + event.output_names
         elif isinstance(event, MCOProgressEvent):
-            self._values.append((event.input[0], event.output[0]))
+            self._values.append(event.input + event.output)
         elif isinstance(event, MCOFinishEvent):
             self._submit_data(self._values)
         else:
@@ -32,31 +32,54 @@ class ExampleNotificationListener(BaseNotificationListener):
 
     def _submit_data(self, values):
         """Submits the data to the GRANTA database"""
-        curdate = datetime.datetime.now().strftime('%Y-%m-%d')
-        packet = {}
-        for index, value in enumerate(values):
-            record_name = curdate+"-"+str(index)
-            packet[self._model.analysis_date_attribute_name] = curdate
-            packet["KPI 1"] = value[0]
-            packet["KPI 2"] = value[1]
-            mi.storeResults(
-                self._session,
-                self._model.url,
-                self._model.db_key,
-                self._model.test_results_table_name,
-                self._model.test_results_import_folder_name,
-                self._model.test_results_subset_name,
-                record_name,
-                packet)
+        model = self._model
+        table = self._mi.get_db(db_key=self._model.db_key).get_table(
+            self.model._table_name)
+
+        curdate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:S')
+        parent = table
+        sn = "Optimization_"+curdate
+
+        with self._mi.make_writer() as writer:
+            data = {}
+            writer.make_record(
+                sn, parent, data, subset_names=[self._model.subset_name])
+
+        parent = table.records_with_name(sn)[0]
+        with self._mi.make_writer() as writer:
+            for index, row in enumerate(self._values):
+                if len(row) != len(self._names):
+                    raise ValueError(
+                        "Inconsistent size between names and values")
+
+                named_row = dict(zip(self._names, row))
+                point = "Point_"+str(index)
+                data = {
+                    "Concentration e": named_row[model.concentration_e_name],
+                    "Material Cost": named_row[model.material_cost_name],
+                    "Production Cost": named_row[model.production_cost_name],
+                    "Date of Analysis": curdate,
+                    "Reaction Time": named_row[model.reaction_time_name],
+                    "Volume a tilde": named_row[model.volume_a_tilde_name],
+                    "Impurity concentration":
+                        named_row[model.impurity_concentration_name],
+                    "Temperature": named_row[model.temperature_name]
+                }
+
+                writer.make_record(
+                    point,
+                    parent,
+                    data,
+                    subset_names=[self._model.subset_name]
+                )
 
     def initialize(self, model):
         self._model = model
-        self._session = requests.Session()
-        self._session.auth = (model.login, model.password)
-        self._session.headers.update({
-            'content-type': 'text/xml;charset=UTF-8'
-        })
+        self._mi = granta.MI(
+            model.url,
+            user_name=model.login,
+            password=model.password,
+            domain=model.domain)
 
     def finalize(self):
         self._model = None
-        self._session.close()
--- granta_example/example_notification_listener/example_notification_listener_model.py
+++ granta_example/example_notification_listener/example_notification_listener_model.py
@@ -4,23 +4,42 @@ from force_bdss.api import BaseNotificationListenerModel
 
 
 class ExampleNotificationListenerModel(BaseNotificationListenerModel):
-    url = Str('https://force.grantami.com/mi_servicelayer/')
+    url = Str('https://212.44.35.85/mi_servicelayer/')
     login = Str()
     password = Password()
-    db_key = Str('MI_FORCE')
-    test_results_table_name = Str('Test Results')
-    test_results_subset_name = Str("Test Results")
-    test_results_import_folder_name = Str('Runs')
-    analysis_date_attribute_name = Str("Date of Analysis")
+    domain = Str()
+    db_key = Str('MI_Force')
+    table_name = Str('Optimization Tests')
+    subset_name = Str("Optimization Tests")
+
+    # Input params
+    volume_a_tilde_name = Str()
+    concentration_e_name = Str()
+    reaction_time_name = Str()
+    temperature_name = Str()
+
+    # KPIs
+    material_cost_name = Str()
+    production_cost_name = Str()
+    impurity_concentration_name = Str()
 
     def default_traits_view(self):
         return View(
             Item("url"),
             Item("login"),
             Item("password"),
+            Item("domain"),
             Item("db_key"),
-            Item("test_results_table_name"),
-            Item("test_results_subset_name"),
-            Item("test_results_import_folder_name"),
-            Item("analysis_date_attribute_name")
+            Item("table_name"),
+            Item("subset_name"),
+
+            Item("volume_a_tilde_name"),
+            Item("concentration_e_name"),
+            Item("reaction_time_name"),
+            Item("temperature_name"),
+
+            # KPIs
+            Item("material_cost_name"),
+            Item("production_cost_name"),
+            Item("impurity_concentration_name"),
         )
