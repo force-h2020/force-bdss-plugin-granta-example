ea871dbae09b0ca95a0554749b62ffbc909a2f9b
James Johnson <jjohnson@enthought.com>
2018-09-26 10:37:26 +0100

    Updates to example_data_source

--- granta_example/example_data_source/example_data_source.py
+++ granta_example/example_data_source/example_data_source.py
@@ -1,9 +1,8 @@
 import logging
 import granta
 
-from traits.api import Instance, Any
+from traits.api import Any
 from force_bdss.api import BaseDataSource, Slot, DataValue
-from granta_example.example_data_source.example_data_source_model import ExampleDataSourceModel
 
 logger = logging.getLogger(__name__)
 
@@ -14,32 +13,30 @@ class ExampleDataSource(BaseDataSource):
 
     def run(self, model, parameters):
 
-        # model = self._model
-
-        # Set up MI database instance
-        self._mi = granta.MI(model.url,
-                             user_name=model.login,
-                             password=model.password,
-                             domain=model.domain)
-
-        table = self._mi.get_db(db_key=model.db_key.get_table(model.source_data_table_name))
-
-        data_records = table.records_with_name('reactor_volume')
-
-        reactor_volume = data_records.get_attribute('reactor_volume')
-        
-        return [
-            DataValue(
-                type=model.cuba_type_out,
-                value=reactor_volume
-            )]
+        mi = granta.MI(
+            model.url, user_name=model.login, password=model.password,
+            domain=model.domain
+        )
+        my_table = mi.get_db(db_key=model.db_key).get_table(model.table_name)
+        reaction_list = list(my_table.records_where((
+            "Product", granta.SearchOps.CONTAINS, 'P')))
+        reaction = reaction_list[0]
+        attribute_names = []
+        for attribute in reaction.attributes:
+            attribute_names.append(attribute.name)
+            if attribute.name == "Arrhenius DeltaH main":
+                delta_h_main = attribute.value[0]
+        if "Arrhenius DeltaH main" not in attribute_names:
+            raise ValueError('Arrhenius DeltaH main was not '
+                             'found in the external database')
+        return [DataValue(type="ARRHENIUS_DELTA_H", value=delta_h_main)]
 
     def slots(self, model):
         return (
-            (
-            ),
-            (
-                Slot(type='VOLUME',
-                     description='Reactor Volume'),
+            tuple(),
+            tuple(Slot(type=model.cuba_type_out,
+                       description="Arrhenius delta H parameter "
+                                 "for the main reaction"
+                       ),
+                 )
             )
-        )
